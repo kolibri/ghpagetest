bbfeef957da6fbebd70d7ca5c7055ccee6c62b6a
ko <entengelb@gmail.com>
2017-01-07 13:00:50 +0100

    foo

---
+++ slider.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+var slides = {
+    "slides/foo001.html",
+    "slides/foo002.html",
+    "slides/foo003.html",
+    "slides/foo004.html",
+    "slides/foo005.html"
+}
+
+var foo = {{ site.github.repository_name }}
+
+
+
+})
